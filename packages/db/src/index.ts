b9c32dc0336a381d8f2b0800e98a5f46399afc2e
Mustafa Sayyed <mustafasayyed2429@gmail.com>
2026-06-08 01:58:09 +0530

    fix drizzle config and user schema

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,1 +1,15 @@
-export {db} from "./db"
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { account, session, user, verification } from "./schema/user.model.js";
+
+export const db = drizzle(process.env.DATABASE_URL!, {
+  casing: "snake_case",
+});
+
+
+export const schema = {
+  user,
+  session,
+  account,
+  verification,
+};
--- packages/db/src/schema/user.model.ts
+++ packages/db/src/schema/user.model.ts
@@ -1,7 +1,7 @@
 import { pgTable } from "drizzle-orm/pg-core";
 import * as t from "drizzle-orm/pg-core";
 
-const users = pgTable("users", {
+export const user = pgTable("user", {
   id: t.uuid().primaryKey().defaultRandom(),
   name: t.text().notNull(),
   email: t.varchar({ length: 255 }).unique().notNull(),
@@ -14,9 +14,9 @@ const users = pgTable("users", {
 export const session = pgTable("session", {
   id: t.uuid().primaryKey().defaultRandom(),
   userId: t
-    .text("user_id")
+    .uuid("user_id")
     .notNull()
-    .references(() => users.id, { onDelete: "cascade" }),
+    .references(() => user.id, { onDelete: "cascade" }),
   token: t.varchar({ length: 255 }).notNull().unique(),
   ipAddress: t.text("ip_address"),
   userAgent: t.text("user_agent"),
@@ -28,16 +28,21 @@ export const session = pgTable("session", {
 export const account = pgTable("account", {
   id: t.uuid().primaryKey().defaultRandom(),
   userId: t
-    .text("user_id")
+    .uuid("user_id")
     .notNull()
-    .references(() => users.id, { onDelete: "cascade" }),
+    .references(() => user.id, { onDelete: "cascade" }),
   accountId: t.text("account_id").notNull(),
   providerId: t.text("provider_id").notNull(),
   accessToken: t.text("access_token"),
-  createdAt: t
-    .timestamp("created_at", { precision: 6, withTimezone: true })
-    .notNull(),
-  updatedAt: t
-    .timestamp("updated_at", { precision: 6, withTimezone: true })
-    .notNull(),
+  createdAt: t.timestamp("created_at", { withTimezone: true }).notNull(),
+  updatedAt: t.timestamp("updated_at", { withTimezone: true }).notNull(),
+});
+
+export const verification = pgTable("verification", {
+  id: t.uuid().primaryKey().defaultRandom(),
+  identifier: t.text("identifier").notNull(),
+  value: t.text("value").notNull(),
+  expiresAt: t.timestamp("expires_at", { withTimezone: true }).notNull(),
+  createdAt: t.timestamp("created_at", { withTimezone: true }).notNull(),
+  updatedAt: t.timestamp("updated_at", { withTimezone: true }).notNull(),
 });
