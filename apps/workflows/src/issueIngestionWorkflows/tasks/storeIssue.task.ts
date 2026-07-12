77423e82216c043bfb4b70f4a9b1f44c226c21f8
mustafa-sayyed <mustafasayyed2429@gmail.com>
2026-07-12 14:36:04 +0530

    refactor: enhance error handling in storeIssueTask

--- apps/workflows/src/issueIngestionWorkflows/tasks/storeIssue.task.ts
+++ apps/workflows/src/issueIngestionWorkflows/tasks/storeIssue.task.ts
@@ -1,6 +1,7 @@
 import { task } from "@renderinc/sdk/workflows";
 import { db, schema } from "../../lib/db.js";
 import type { GitHubIssueSearchItem } from "../../types/github.types.js";
+import { NeonDbError } from "@neondatabase/serverless";
 
 /**
  * Task: Persist a GitHub issue, its embedding into the database.
@@ -14,23 +15,46 @@ export const storeIssueTask = task(
     githubRepoId: string,
     embedding: number[],
   ) => {
-    await db.insert(schema.issue).values({
-      githubRepoId,
-      githubId: item.id,
-      title: item.title,
-      body: item.body ?? "",
-      url: item.html_url,
-      status: "open",
-      embedding,
-    });
+    try {
+      await db.insert(schema.issue).values({
+        githubRepoId,
+        githubId: item.id,
+        title: item.title,
+        body: item.body ?? "",
+        url: item.html_url,
+        status: "open",
+        embedding,
+      });
 
-    console.log(`Issue #${item.id} — "${item.title}" stored successfully.`);
+      console.log(`Issue #${item.id} — "${item.title}" stored successfully.`);
 
-    return {
-      success: true,
-      issueId: item.id,
-      issueTitle: item.title,
-      issueUrl: item.html_url,
-    };
+      return {
+        success: true,
+        issueId: item.id,
+        issueTitle: item.title,
+        issueUrl: item.html_url,
+      };
+    } catch (error) {
+      if (
+        error instanceof NeonDbError &&
+        (error.code === "23505" || error?.constraint === "issue_url_unique")
+      ) {
+        console.log("Duplicate issue, issue already exist in the DB", error);
+        return {
+          success: false,
+          error: "Duplicate issue, issue already exist in the DB",
+        };
+      }
+
+      console.error(
+        `Failed to store issue #${item.id} — "${item.title}":`,
+        error,
+      );
+
+      return {
+        success: false,
+        error: (error as Error).message,
+      };
+    }
   },
 );
