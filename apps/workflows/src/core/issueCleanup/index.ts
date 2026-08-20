946a2fe908c3b9148378fc89c893659d45e87596
mustafa-sayyed <mustafasayyed2429@gmail.com>
2026-08-20 22:22:31 +0530

    feat(workflows): add issue cleanup functions

---
+++ apps/workflows/src/core/issueCleanup/index.ts
@@ -0,0 +1,3 @@
+export { checkGithubIssueStatusTask } from "./checkGithubIssueStatus.js";
+export { updateIssueStatusTask } from "./updateIssueStatus.js";
+export { fetchCleanupCandidatesTask } from "./fetchCleanupCandidates.js";
