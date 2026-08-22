7d338bc51d3d65bcfd16c402ce29b67fa4a3bc8f
mustafa-sayyed <mustafasayyed2429@gmail.com>
2026-08-23 01:04:42 +0530

    refactor(workflows): remove logger from Inngest configuration

--- apps/workflows/src/inngest/client.ts
+++ apps/workflows/src/inngest/client.ts
@@ -1,7 +1,5 @@
-import { WorkflowLogger } from "@packages/logging";
 import { Inngest } from "inngest";
 
 export const inngest = new Inngest({
   id: "getissues-workflows",
-  logger: WorkflowLogger,
 });
