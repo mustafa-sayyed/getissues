437e59115f2643ded9bc29223f6185187b13c2ae
mustafa-sayyed <mustafasayyed2429@gmail.com>
2026-09-17 23:53:00 +0530

    feat(web): setup posthog proxy

--- apps/web/components/posthog/PostHogProvider.tsx
+++ apps/web/components/posthog/PostHogProvider.tsx
@@ -5,7 +5,8 @@ import { PostHogProvider } from "posthog-js/react";
 
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_API_HOST,
+    ui_host: process.env.NEXT_PUBLIC_POSTHOG_UI_HOST,
     capture_pageview: true,
     autocapture: true,
   });
